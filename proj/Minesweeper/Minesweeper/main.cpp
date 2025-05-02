ce34551e93e10db570274bb719b4389c2147a317
skiper9812 <skiper9812@gmail.com>
2025-05-02 19:19:54 +0200

    Working SFML 3.0

--- proj/Minesweeper/Minesweeper/main.cpp
+++ proj/Minesweeper/Minesweeper/main.cpp
@@ -1,7 +1,26 @@
-#include "Klasa.h"
+#include <SFML/Graphics.hpp>
+#include <optional>  // for std::optional
 
-int main() {
-	Klasa k;
-	k.test();
-	return 0;
+int main()
+{
+    // 1) Create the window
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML 3.0 Test");
+
+    // 2) Main loop
+    while (window.isOpen())
+    {
+        // 3) Poll events (std::optional<sf::Event>)
+        while (auto event = window.pollEvent())
+        {
+            // 4) Check for close event
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // 5) Clear and display
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
 }
